16aed5a67830c7a1bad79b176e7094d3ca0d8ae5
s-degu <s.degu15@gmail.com>
2022-01-26 13:58:30 +0900

    page-about.php | sp ファーストビュー

--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -1,3 +1,8 @@
+<?php
+  $home = esc_url(home_url('/'));
+  $about = esc_url(home_url('/about'));
+?>
+
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 
@@ -20,15 +25,15 @@
       <div class="p-header__right">
         <ul class="p-header__nav-wrap">
           <li class="p-header__nav">
-            <a class="p-header__nav-link" href="#">
+            <a class="p-header__nav-link" href="<?php echo $home?>">
             <svg class="p-header__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><rect width="24" height="24" fill="#393939" opacity="0"/><path d="M20.42,10.18,12.71,2.3a1,1,0,0,0-1.42,0L3.58,10.19A2,2,0,0,0,3,11.62V20a2,2,0,0,0,1.89,2H19.11A2,2,0,0,0,21,20V11.62a2.07,2.07,0,0,0-.58-1.44ZM10,20V14h4v6Zm9,0H16V13a1,1,0,0,0-1-1H9a1,1,0,0,0-1,1v7H5V11.58l7-7.15,7,7.19Z" fill="#393939"/></svg>
-            <span href="#" >ホーム</span>
+            <span>ホーム</span>
             </a>
           </li>
           <li class="p-header__nav">
-            <a class="p-header__nav-link" href="#">
+            <a class="p-header__nav-link" href="<?php echo $about?>">
             <svg class="p-header__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><rect width="24" height="24" fill="#393939" opacity="0"/><path d="M5.368,10.232V8.555a.5.5,0,0,1,.5-.5H7.548a.5.5,0,0,1,.5.5v1.677a.5.5,0,0,1-.5.5H5.871A.5.5,0,0,1,5.368,10.232Zm5.871.5h1.677a.5.5,0,0,0,.5-.5V8.555a.5.5,0,0,0-.5-.5H11.238a.5.5,0,0,0-.5.5v1.677A.5.5,0,0,0,11.238,10.735ZM8.051,14.258V12.58a.5.5,0,0,0-.5-.5H5.871a.5.5,0,0,0-.5.5v1.677a.5.5,0,0,0,.5.5H7.548A.5.5,0,0,0,8.051,14.258Zm3.187.5h1.677a.5.5,0,0,0,.5-.5V12.58a.5.5,0,0,0-.5-.5H11.238a.5.5,0,0,0-.5.5v1.677A.5.5,0,0,0,11.238,14.761Zm7.548,5.2v1.51H0v-1.51a.5.5,0,0,1,.5-.5h.818V3.566a.95.95,0,0,1,1.006-.882H6.039V1.006A1.006,1.006,0,0,1,7.045,0h4.7a1.006,1.006,0,0,1,1.006,1.006V2.684h3.711a.95.95,0,0,1,1.006.882V19.458h.818A.5.5,0,0,1,18.787,19.961ZM3.334,19.416H8.051v-2.81a.5.5,0,0,1,.5-.5h1.677a.5.5,0,0,1,.5.5v2.81h4.718V4.7h-2.7V5.7A1.006,1.006,0,0,1,11.742,6.71h-4.7A1.006,1.006,0,0,1,6.039,5.7V4.7h-2.7ZM11.155,2.684h-1.09V1.594a.252.252,0,0,0-.252-.252H8.974a.252.252,0,0,0-.252.252v1.09H7.632a.252.252,0,0,0-.252.252v.839a.252.252,0,0,0,.252.252h1.09v1.09a.252.252,0,0,0,.252.252h.839a.252.252,0,0,0,.252-.252V4.026h1.09a.252.252,0,0,0,.252-.252V2.935A.252.252,0,0,0,11.155,2.684Z" transform="translate(3 1)" fill="#393939"/></svg>
-            <span href="#" >当院について</span>
+            <span>当院について</span>
             </a>
           </li>
           <li class="p-header__nav">
@@ -79,8 +84,8 @@
           <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-drawer.png" alt="みなみ歯科クリニック">
         </a>
         <ul class="p-drawer__menus">
-          <li class="p-drawer__menu p-drawer__menu--home"><a href="#">ホーム</a></li>
-          <li class="p-drawer__menu p-drawer__menu--about"><a href="#">当院について</a></li>
+          <li class="p-drawer__menu p-drawer__menu--home"><a href="<?php echo $home?>">ホーム</a></li>
+          <li class="p-drawer__menu p-drawer__menu--about"><a href="<?php echo $about?>">当院について</a></li>
           <li class="p-drawer__menu p-drawer__menu--info"><a href="#">診療案内</a></li>
           <li class="p-drawer__menu p-drawer__menu--staff"><a href="#">スタッフ紹介</a></li>
           <li class="p-drawer__menu p-drawer__menu--blog"><a href="#">スタッフブログ</a></li>
@@ -90,4 +95,3 @@
       <!-- 👆ドロワーメニュー -->
     </div>
   </header>
-
---
+++ WordPressTheme/page-about.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+
+<!-- ファーストビュー -->
+<div class="l-sub-fv p-sub-fv">
+  <div class="l-inner p-sub-fv__inner p-sub-fv__inner--about">
+    <div class="p-sub-fv__content">
+      <div class="p-sub-fv__head">
+        <h1 class="p-sub-fv__title">当院について</h1>
+        <span class="p-sub-fv__sub-title">ABOUT OUR CLINIC</span>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- ファーストビュー -->
+
+<!-- パンくず -->
+<div class="l-breadcrumb p-breadcrumb">
+  <div class="l-inner p-breadcrumb__inner">
+    <span property="itemListElement" typeof="ListItem">
+	<a property="item" typeof="WebPage" title="Go to codeups." href="#" class="home" >
+		<span property="name">ホーム</span>
+	</a>
+	<meta property="position" content="1">
+</span> &gt;
+
+<span property="itemListElement" typeof="ListItem">
+	<span property="name" class="post-root post post-post current-item">当院について</span>
+	<meta property="url" content="http://codeups.local/archive/">
+	<meta property="position" content="2">
+</span>
+  </div>
+</div>
+<!-- パンくず -->
+
+
+
+<?php get_footer(); ?>
