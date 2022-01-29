2143e2c3a2e6e9ccbda10385d62f2fa8fd9f89da
s-degu <s.degu15@gmail.com>
2022-01-29 12:29:41 +0900

    page-about.php | pc

--- WordPressTheme/page-about.php
+++ WordPressTheme/page-about.php
@@ -39,16 +39,17 @@
       <h2 class="p-about-policy__title c-section__title">ポリシーと特徴</h2>
     </div>
     <div class="p-policy__content">
-      <div class="p-policy__deco"></div>
-      <div class="p-policy__wrap">
-        <div class="p-policy__textarea">
-          <span class="p-policy__sub-title">policy</span>
-          <h2 class="p-policy__title">コミュニケーションから始まる最適な医療提供</h2>
-          <p class="p-policy__text">当院ではまず患者様から詳しくお話を伺います。<br>難しい言葉は使わず、実際に感じている小さな違和感からあらゆる可能性を考え、最適な治療方法をご提案いたします。</p>
-          <p class="p-policy__text"> 「どこよりも本音で話せる歯医者さん」を目指し、スタッフ一同、「人間力の向上」にも勤めております。</p>
-        </div>
-        <div class="p-policy__img">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/feature_1.jpg" alt="診療の様子">
+      <div class="p-policy__deco">
+        <div class="p-policy__wrap">
+          <div class="p-policy__textarea">
+            <span class="p-policy__sub-title">policy</span>
+            <h2 class="p-policy__title">コミュニケーションから始まる最適な医療提供</h2>
+            <p class="p-policy__text">当院ではまず患者様から詳しくお話を伺います。<br>難しい言葉は使わず、実際に感じている小さな違和感からあらゆる可能性を考え、最適な治療方法をご提案いたします。</p>
+            <p class="p-policy__text"> 「どこよりも本音で話せる歯医者さん」を目指し、スタッフ一同、「人間力の向上」にも勤めております。</p>
+          </div>
+          <div class="p-policy__img">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/feature_1.jpg" alt="診療の様子">
+          </div>
         </div>
       </div>
     </div>
