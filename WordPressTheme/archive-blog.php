07c9b467ce161601f439aaa472c1942127603395
s-degu <s.degu15@gmail.com>
2022-02-02 20:21:38 +0900

    archive-blog.php | sp 新着記事、カテゴリー

--- WordPressTheme/archive-blog.php
+++ WordPressTheme/archive-blog.php
@@ -173,20 +173,106 @@
           <?php if(function_exists('wp_pagenavi')) wp_pagenavi(array('query' => $my_query));?>
         </div>
       </div>
-      <aside class="p-sub-blog__sideber p-sidebar">
-          <div class="p-sidebar__block p-sidebar__clinic">
-            <h2 class="p-sidebar__title">
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-ciinic.svg" alt="">
-              クリニックの紹介
-            </h2>
-            <div class="p-sidebar-clinic__img">
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/side-clinic.jpg" alt="">
-            </div>
-            <h3 class="p-sidebar__sub-title">みなみ歯科クリニック</h3>
-            <p class="p-sidebar-clinic__text">お子様からご高齢の方まで、快適な空間で治療が受けられる場を作り、地域医療に貢献しきたいと考えております。</p>
-            <a href="#" class="p-sidebar-clinic__button">当院について</a>
+      <aside class="l-sub-blog__sideber p-sub-blog__sideber p-sidebar">
+        <div class="p-sidebar__block p-sidebar__clinic">
+          <h2 class="p-sidebar__title">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-ciinic.svg" alt="">
+            クリニックの紹介
+          </h2>
+          <div class="p-sidebar-clinic__img">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/side-clinic.jpg" alt="">
           </div>
-
+          <h3 class="p-sidebar__sub-title">みなみ歯科クリニック</h3>
+          <p class="p-sidebar-clinic__text">お子様からご高齢の方まで、快適な空間で治療が受けられる場を作り、地域医療に貢献しきたいと考えております。</p>
+          <a href="#" class="p-sidebar-clinic__button">当院について</a>
+        </div>
+        <div class="l-new-article p-sidebar__new-article p-new-article">
+          <h2 class="p-sidebar__title">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-article.svg" alt="">
+            新着記事
+          </h2>
+          <div class="l-new-article__cards p-sub-blog__items p-blog-cards">
+            <article class="p-blog-cards__item p-blog-card">
+              <a class="p-blog-card__link" href="#">
+                <div class="p-blog-card__img">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog01.jpg" alt="">
+                </div>
+                <div class="p-blog-card__body">
+                  <span class="p-blog-card__label">お知らせ</span>
+                  <p class="p-blog-card__text">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time datetime="2020-02-14" class="p-blog-card__date">2020.02.14</time>
+                </div>
+              </a>
+            </article>
+            <article class="p-blog-cards__item p-blog-card">
+              <a class="p-blog-card__link" href="#">
+                <div class="p-blog-card__img">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog01.jpg" alt="">
+                </div>
+                <div class="p-blog-card__body">
+                  <span class="p-blog-card__label">お知らせ</span>
+                  <p class="p-blog-card__text">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time datetime="2020-02-14" class="p-blog-card__date">2020.02.14</time>
+                </div>
+              </a>
+            </article>
+            <article class="p-blog-cards__item p-blog-card">
+              <a class="p-blog-card__link" href="#">
+                <div class="p-blog-card__img">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog01.jpg" alt="">
+                </div>
+                <div class="p-blog-card__body">
+                  <span class="p-blog-card__label">お知らせ</span>
+                  <p class="p-blog-card__text">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time datetime="2020-02-14" class="p-blog-card__date">2020.02.14</time>
+                </div>
+              </a>
+            </article>
+            <article class="p-blog-cards__item p-blog-card">
+              <a class="p-blog-card__link" href="#">
+                <div class="p-blog-card__img">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog01.jpg" alt="">
+                </div>
+                <div class="p-blog-card__body">
+                  <span class="p-blog-card__label">お知らせ</span>
+                  <p class="p-blog-card__text">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time datetime="2020-02-14" class="p-blog-card__date">2020.02.14</time>
+                </div>
+              </a>
+            </article>
+            <article class="p-blog-cards__item p-blog-card">
+              <a class="p-blog-card__link" href="#">
+                <div class="p-blog-card__img">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/blog01.jpg" alt="">
+                </div>
+                <div class="p-blog-card__body">
+                  <span class="p-blog-card__label">お知らせ</span>
+                  <p class="p-blog-card__text">記事のタイトルが入ります。記事のタイトルが入ります…</p>
+                  <time datetime="2020-02-14" class="p-blog-card__date">2020.02.14</time>
+                </div>
+              </a>
+            </article>
+          </div>
+          <div class="p-sub-blog__category p-blog-categry l-blog-categry">
+            <h2 class="p-sidebar__title">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-category.svg" alt="">
+              カテゴリー
+            </h2>
+            <ul class="p-blog-category__lists">
+              <li class="p-blog-categpry__item">
+                <a href="#">テキストテキスト</a>
+              </li>
+              <li class="p-blog-categpry__item">
+                <a href="#">テキストテキスト</a>
+              </li>
+              <li class="p-blog-categpry__item">
+                <a href="#">テキストテキスト</a>
+              </li>
+              <li class="p-blog-categpry__item">
+                <a href="#">テキストテキスト</a>
+              </li>
+            </ul>
+          </div>
       </aside>
     </div>
   </div>
